82d3ecaa50d0a3ea77193d6c6e2baf1fda61a63f
Drake <drake.ponte@hotmail.com>
2026-01-26 14:56:23 -0500

    Added FPS clock

--- main.py
+++ main.py
@@ -9,15 +9,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while 1 == 1:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 
