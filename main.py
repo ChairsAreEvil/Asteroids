3924d10651eee2b489febb6ad45cb02ab73785b7
Drake <drake.ponte@hotmail.com>
2026-01-26 18:22:36 -0500

    Add shot collision and asteroid splitting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 #screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -20,10 +21,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
 
     field = AsteroidField()
     ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
@@ -38,11 +41,20 @@ def main():
         
         screen.fill("black")
         updatable.update(dt)
+        
         for asteroid in asteroids:
             if asteroid.collides_with(ship):
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+
         for draw in drawable:
             draw.draw(screen)
         pygame.display.flip()
